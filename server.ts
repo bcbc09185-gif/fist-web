168b757473617f8660ca238b72ce5e60f1d30b31
bcbc09185-gif <bcbc09185@gmail.com>
2026-05-23 12:42:35 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -206,7 +206,11 @@ async function seedDefaultConfig() {
 }
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
