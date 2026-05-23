9e8b7aebc37376c949c67fe9039bca632f444ed8
bcbc09185-gif <bcbc09185@gmail.com>
2026-05-23 12:30:20 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,9 @@ export default defineConfig(({mode}) => {
     base: '/websitebazer/',
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
+  'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+  'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+},
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
